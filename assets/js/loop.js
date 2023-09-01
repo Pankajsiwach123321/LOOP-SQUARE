8bd82a44cc0ea5d3b9a59b28447d506456e08a5f
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-09-01 14:05:27 +0530

    add css decoration

--- assets/js/loop.js
+++ assets/js/loop.js
@@ -4,3 +4,10 @@ for (let x = 1, y = 1; x, y < 31; x = (x * 2), y++) {
     para.innerHTML = x;
     document.querySelector('#para').appendChild(para);
 }
+for (let t = 0; t < 21; t += 2) {
+    console.log(t);
+        let para2 = document.createElement('p');
+        para2.innerHTML = t;
+        document.querySelector('#para2').appendChild(para2);
+    
+}
